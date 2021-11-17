b2e24f8046446425a420c0f9be9402ba930e09b5
daun <daun@DESKTOP-OV1PKLJ>
2021-11-17 15:43:42 +0900

    반복문중간저장2

---
+++ src/com/javaex/ex04/Ex07.java
@@ -0,0 +1,16 @@
+package com.javaex.ex04;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		for(int ln=1; ln<=6; ln++) {
+			for(int i=1; i<=5; i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,19 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		
+		for(int ln=1; ln<=6; ln++) {
+			for(int i=1; i<=num; i++) {
+				System.out.print("*");
+			}
+			num++;
+			System.out.println("");
+		}
+
+	}
+
+}
