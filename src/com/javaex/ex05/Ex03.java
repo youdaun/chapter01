6a00d75307d030a99124babdc50a6bdc981f400f
youdaun <yjyou960@naver.com>
2021-11-19 10:48:47 +0900

    커밋테스트2

---
+++ src/com/javaex/ex05/Ex03.java
@@ -0,0 +1,24 @@
+package com.javaex.ex05;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		int[] intArray = new int[3];
+		
+		intArray[0] = 3;
+		intArray[2] = 13;
+		intArray[1] = 7;
+		
+		System.out.println(intArray[1]);
+		
+		for(int i=0; i<3; i++) {
+			System.out.println(intArray[i]);
+		}
+		
+		
+		System.out.println("============================");
+
+	}
+
+}
